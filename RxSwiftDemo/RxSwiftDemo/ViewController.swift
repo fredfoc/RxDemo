96f7097d996228e07b3c1fc291be107edd2a2b6c
fred fauquette <fredfocmac@gmail.com>
2016-10-18 13:53:25 +0200

    add some Subject test

--- RxSwiftDemo/RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/RxSwiftDemo/ViewController.swift
@@ -27,6 +27,44 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let subject = PublishSubject<String>()
+        subject
+            .subscribe(onNext: { (value) in print("value is \(value)") },
+                       onError: { error in print("error")},
+                       onCompleted: { print("completed")},
+                       onDisposed: { print("disposed")})
+            .addDisposableTo(disposeBag)
+        
+        subject.onNext("start")
+        
+        
+        subject
+            .map { (value) -> String in
+                return "value is \(value)"
+            }
+            .bindTo(myLabel.rx.text)
+            .addDisposableTo(disposeBag)
+        
+        tapMeButton.rx.tap
+            .asObservable()
+            .subscribe(onNext: { [weak self] () in
+                self?.count += 1
+                if let count = self?.count {
+                    if count == 10 {
+                        subject.onError(MyError.subjectError)
+                    } else {
+                        subject.onNext("value : \(count)")
+                    }
+                    print("tap : \(count)")
+                }
+                
+                },
+                       onError: nil,
+                       onCompleted: nil,
+                       onDisposed: nil)
+            .addDisposableTo(disposeBag)
+        
     }
 }
 
